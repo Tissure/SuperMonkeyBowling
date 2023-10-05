a1dda9a638dac39e74eaab356778d5cca24b8474
Kyle <kyle.ng7512@gmail.com>
2023-10-04 20:54:50 -0700

    Update main workflow

---
+++ Source/SuperMonkeyBowling/SuperMonkeyBowling.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SuperMonkeyBowling.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SuperMonkeyBowling, "SuperMonkeyBowling" );
