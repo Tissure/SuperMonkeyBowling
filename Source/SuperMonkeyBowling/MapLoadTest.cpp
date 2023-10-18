08d6d1ef78b1e023acdfe3c83bf229386ae57b4e
unknown <jwang550@my.bcit.ca>
2023-10-17 18:20:11 -0700

    Added Automated Test: MapLoadTest for Kyle's Map

---
+++ Source/SuperMonkeyBowling/MapLoadTest.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapLoadTest.h"
+#include "Misc/AutomationTest.h"
+#include <Tests/AutomationEditorCommon.h>
+#include <FileHelpers.h>
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(mapLoadTest, "MapLoadTest", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool mapLoadTest::RunTest(const FString& Parameters) {
+
+	// Providing the name of the Map.
+	FString MapName = TEXT("/Game/Maps/KyleNWorkspace.KyleNWorkspace");
+
+
+	bool bLoadAsTemplate = false;
+	bool bShowProgress = false;
+	return FEditorFileUtils::LoadMap(MapName, bLoadAsTemplate, bShowProgress);
+
+	// Testing the loading of the map.
+	//FAutomationEditorCommonUtils::LoadMap(MapName);
+
+
+	//return true;
+}
+
+MapLoadTest::MapLoadTest()
+{
+}
+
+MapLoadTest::~MapLoadTest()
+{
+}
